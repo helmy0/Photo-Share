6e08431f2763e047608707ef84931654ad3b326e
Omar Farrag <omar.tarek.farrag123@gmail.com>
2024-02-25 21:07:31 +0000

    update

--- photos/views.py
+++ photos/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-
+from .models import Category, Photo
 # Create your views here.
 
 
 def gallery(request):
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
 
-    return render(request, "photos/gallery.html" )
+    context = {
+        'categories': categories,
+        'photos': photos,
+    }
+    return render(request, "photos/gallery.html", context)
 
 def viewPhoto(request, pk):
-
-    return render(request, "photos/photo.html" )
+    photo = Photo.objects.get(id=pk)
+    return render(request, "photos/photo.html", {'photo': photo})
 
 def addPhoto(request):
 
